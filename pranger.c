fd23ff4b80bbdf59b3c05fc03b1b6b2b8f9eceeb
brew <brew@cocaine.ninja>
2026-07-01 17:08:50 -0700

    finish initial pranger impl

--- pranger.c
+++ pranger.c
@@ -1,7 +1,10 @@
 #include <time.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
 #include "parser.h"
 
 uint32_t weight = 0;
@@ -14,6 +17,53 @@ uint32_t height = 0;
 
 uint8_t augment_rref() {
     rref_aug_mat = malloc((width+1)*height);
+
+    // augment
+    for (uint16_t a = 0; a < height; a++) {
+        for (uint16_t b = 0; b < width; b++) {
+            rref_aug_mat[a*(width+1)+b] = permutated_mat[a*width+b]; 
+        }
+        rref_aug_mat[a*(width+1)+width] = syndrome[a];
+    }
+
+    // attempt rref
+    uint16_t swap_ptr;
+    bool found = false;
+    uint8_t tmp_coord;
+
+
+    for (uint16_t a = 0; a < height; a++) {
+        found = false;
+        for (uint16_t b = a; b < height; b++) {
+            if (rref_aug_mat[b*(width+1)+a]) {
+                found = true;
+                swap_ptr = b;
+                break;
+            }
+        }
+        if (!found) {
+            return 1;
+        }
+
+        // vector at a = vector at swap_ptr
+        for (uint16_t b = 0; b < width+1; b++) {
+            tmp_coord = rref_aug_mat[a*(width+1)+b];
+            rref_aug_mat[a*(width+1)+b] = rref_aug_mat[swap_ptr*(width+1)+b];
+            rref_aug_mat[swap_ptr*(width+1)+b] = tmp_coord; 
+        }
+
+        // actual reduction
+        for (uint16_t b = 0; b < height; b++) {
+            if (rref_aug_mat[b*(width+1)+a] && b != a) {
+                for (uint16_t c = 0; c < width+1; c++) {
+                    rref_aug_mat[b*(width+1)+c] ^= rref_aug_mat[a*(width+1)+c]; 
+                }       
+            }
+        }
+
+    }
+
+    return 0;
 }
 
 uint16_t* permutation_gen() {
@@ -47,7 +97,6 @@ void apply_permutation(uint16_t* permutation) {
 
 int main(int argc, char *argv[]) {
     uint16_t seed = time(NULL);
-    srand(seed);
     
     if (argc != 4) {
         printf("Usage: %s <matrix.txt> <syndrome.txt> <target weight>\n", argv[0]);
@@ -56,42 +105,49 @@ int main(int argc, char *argv[]) {
     }
 
     o_parse(argv);
-
-    printf("%u %u\n", width, height);
-    for (int i = 0; i < width*height; i++) {
-        printf("%u", parity_mat[i]);
-        if (!((i+1)%width)) {
-            putchar('\n');
+    uint16_t best_weight = 0;
+    long cores = sysconf(_SC_NPROCESSORS_ONLN);
+    for (int i = 0; i < cores; i++) {
+        srand(seed+i);
+        if (!fork()) {
+            break;
         }
     }
-    putchar('\n');
-
-    for (int i = 0; i < height; i++) {
-        printf("%u", syndrome[i]);
-    }
-    putchar('\n');
-
-    printf("%u\n", weight);
-
+    retry:
     uint16_t *permutation = permutation_gen();
-    for (int i = 0; i < width; i++) {
-        printf("%hu ", permutation[i]);
-    }
-    putchar('\n');
-
     apply_permutation(permutation);
-    for (int i = 0; i < width*height; i++) {
-        printf("%hu", permutated_mat[i]);
-        if (!((i+1)%width)) {
-            putchar('\n');
-        }
-    }
-    putchar('\n');
 
     while (augment_rref()) {
+        free(rref_aug_mat);
         free(permutation);
         permutation = permutation_gen();
         free(permutated_mat);
         apply_permutation(permutation);
     }
-}
+    
+    uint8_t* sol = malloc(width*sizeof(uint8_t));
+    memset(sol, 0, width*sizeof(uint8_t));
+    for (uint16_t a = 0; a < height; a++) {
+        sol[permutation[a]] = rref_aug_mat[(width+1)*a+(width)];
+    }
+    uint16_t targ_weight = 0;
+    for (uint16_t a = 0; a < width; a++) {
+        targ_weight += sol[a];
+    }
+    if (!best_weight || best_weight > targ_weight) {
+        printf("%hu\n", targ_weight);
+        best_weight = targ_weight;
+    }
+    if (weight <= targ_weight) {
+        free(rref_aug_mat);
+        free(permutation);
+        free(permutated_mat);
+        goto retry;
+    }
+    putchar('\n');
+    for (uint8_t a = 0; a < width; a++) {
+        printf("%hu", sol[a]);
+    }
+    putchar('\n');
+} 
+
